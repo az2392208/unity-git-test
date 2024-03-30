87656d455554326a6226c01509acfe25cf0a2279
Aive <az2392208@gmail.com>
2024-03-30 17:25:50 +0800

    '测试一下忽略文件的修改'

--- CloneGit.bat
+++ CloneGit.bat
@@ -1,6 +1,7 @@
-git init
-git remote add -f origin https://github.com/az2392208/unity-git-test.git
-git sparse-checkout init
-git sparse-checkout set Art
-git pull origin master
+---git init
+---git remote add -f origin https://github.com/az2392208/unity-git-test.git
+---git sparse-checkout init
+---git sparse-checkout set Art
+---git pull origin master
+git clone https://github.com/az2392208/unity-git-test.git
 pause
