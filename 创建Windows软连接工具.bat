53833407889ef4f76b63f596ee6d6471d288452f
Aive <az2392208@gmail.com>
2024-03-30 15:02:21 +0800

    '软连接测试'

--- 创建Windows软连接工具.bat
+++ 创建Windows软连接工具.bat
@@ -1,14 +1,5 @@
-@ECHO OFF
-setlocal EnableDelayedExpansion
-color 3e
-title 标题
-  
-PUSHD %~DP0 & cd /d "%~dp0"
-%1 %2
-mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :runas","","runas",1)(window.close)&goto :eof
-:runas
-echo Successfully ran as administrator.
-mklink /J ".\Client\ExcelLink" ".\Excel"
-mklink /J ".\Client\MusicLink" ".\Music"
+mklink /J ".\Client\Art" ".\Art"
+mklink /J ".\Client\Excel" ".\Excel"
+mklink /J ".\Client\Music" ".\Music"
 pause >nul
 exit
